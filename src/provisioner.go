94c64d3361da0756c30406f7083d4c0aef7fea57
Arthur Chaloin <arthur@achaloin.com>
2019-05-06 11:53:23 +0200

    feat(provisioner): return placeholder persistent volume from provision call

--- src/provisioner.go
+++ src/provisioner.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"errors"
-	"fmt"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/sig-storage-lib-external-provisioner/controller"
 )
 
@@ -17,8 +17,37 @@ func NewDothillProvisioner() controller.Provisioner {
 }
 
 func (p *dothillProvisioner) Provision(options controller.VolumeOptions) (*v1.PersistentVolume, error) {
-	fmt.Println(options)
-	return nil, errors.New("unimplemented")
+	mode := v1.PersistentVolumeFilesystem
+	iqn := "iqn.2019-05.io.enix:storage00"
+
+	return &v1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: iqn,
+		},
+		Spec: v1.PersistentVolumeSpec{
+			PersistentVolumeReclaimPolicy: options.PersistentVolumeReclaimPolicy,
+			AccessModes:                   options.PVC.Spec.AccessModes,
+			VolumeMode:                    &mode,
+			Capacity: v1.ResourceList{
+				v1.ResourceName(v1.ResourceStorage): options.PVC.Spec.Resources.Requests[v1.ResourceName(v1.ResourceStorage)],
+			},
+			PersistentVolumeSource: v1.PersistentVolumeSource{
+				ISCSI: &v1.ISCSIPersistentVolumeSource{
+					TargetPortal: "1.2.3.4:3260",
+					Portals:      []string{"1.2.3.4:3260"},
+					IQN:          iqn,
+					Lun:          0,
+					FSType:       "ext4",
+					ReadOnly:     false,
+					// DiscoveryCHAPAuth: true,
+					// SessionCHAPAuth: true,
+					// SecretRef: &v1.SecretReference{
+					// 	Name: "chap-secret",
+					// },
+				},
+			},
+		},
+	}, nil
 }
 
 func (p *dothillProvisioner) Delete(*v1.PersistentVolume) error {
