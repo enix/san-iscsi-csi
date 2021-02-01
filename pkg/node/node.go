194b06bf72e71cbd44d1f9ebffcde24ea8d5e445
Paul Laffitte <paul.laffitte@enix.fr>
2021-02-01 21:28:06 +0700

    feat(node): volume expansion

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -59,7 +59,9 @@ func (driver *Driver) NewServerInterceptors(logRoutineServerInterceptor grpc.Una
 
 // ShouldLogRoutine implements DriverImpl.ShouldLogRoutine
 func (driver *Driver) ShouldLogRoutine(fullMethod string) bool {
-	return fullMethod == "/csi.v1.Node/NodePublishVolume" || fullMethod == "/csi.v1.Node/NodeUnpublishVolume"
+	return fullMethod == "/csi.v1.Node/NodePublishVolume" ||
+		fullMethod == "/csi.v1.Node/NodeUnpublishVolume" ||
+		fullMethod == "/csi.v1.Node/NodeExpandVolume"
 }
 
 // NodeGetInfo returns info about the node
@@ -79,7 +81,7 @@ func (driver *Driver) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoReque
 func (driver *Driver) NodeGetCapabilities(ctx context.Context, req *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
 	var csc []*csi.NodeServiceCapability
 	cl := []csi.NodeServiceCapability_RPC_Type{
-		// csi.NodeServiceCapability_RPC_EXPAND_VOLUME,
+		csi.NodeServiceCapability_RPC_EXPAND_VOLUME,
 	}
 
 	for _, cap := range cl {
@@ -158,7 +160,7 @@ func (driver *Driver) NodePublishVolume(ctx context.Context, req *csi.NodePublis
 		return nil, status.Error(codes.Internal, string(out))
 	}
 
-	iscsiInfoPath := fmt.Sprintf("%s/plugins/%s/iscsi-%s.json", driver.kubeletPath, common.PluginName, req.GetVolumeId())
+	iscsiInfoPath := driver.getIscsiInfoPath(req.GetVolumeId())
 	klog.Infof("saving ISCSI connection info in %s", iscsiInfoPath)
 	err = connector.Persist(iscsiInfoPath)
 	if err != nil {
@@ -196,7 +198,7 @@ func (driver *Driver) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpu
 		os.Remove(req.GetTargetPath())
 	}
 
-	iscsiInfoPath := fmt.Sprintf("%s/plugins/%s/iscsi-%s.json", driver.kubeletPath, common.PluginName, req.GetVolumeId())
+	iscsiInfoPath := driver.getIscsiInfoPath(req.GetVolumeId())
 	klog.Infof("loading ISCSI connection info from %s", iscsiInfoPath)
 	connector, err := iscsi.GetConnectorFromFile(iscsiInfoPath)
 	if err != nil {
@@ -224,8 +226,32 @@ func (driver *Driver) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpu
 
 // NodeExpandVolume finalizes volume expansion on the node
 func (driver *Driver) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandVolumeRequest) (*csi.NodeExpandVolumeResponse, error) {
-	fmt.Println("NodeExpandVolume call")
-	return nil, status.Error(codes.Unimplemented, "NodeExpandVolume unimplemented yet")
+	iscsiInfoPath := driver.getIscsiInfoPath(req.GetVolumeId())
+	connector, err := iscsi.GetConnectorFromFile(iscsiInfoPath)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	for i := range connector.Devices {
+		connector.Devices[i].Rescan()
+	}
+
+	if connector.IsMultipathEnabled() {
+		klog.V(2).Info("device is using multipath")
+		if err := iscsi.ResizeMultipathDevice(connector.MountTargetDevice); err != nil {
+			return nil, err
+		}
+	} else {
+		klog.V(2).Info("device is NOT using multipath")
+	}
+
+	klog.Infof("expanding filesystem on device %s", connector.MountTargetDevice.GetPath())
+	output, err := exec.Command("resize2fs", connector.MountTargetDevice.GetPath()).CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("could not resize filesystem: %v", output)
+	}
+
+	return &csi.NodeExpandVolumeResponse{}, nil
 }
 
 // NodeGetVolumeStats return info about a given volume
@@ -259,6 +285,10 @@ func (driver *Driver) Probe(ctx context.Context, req *csi.ProbeRequest) (*csi.Pr
 	}
 
 	return &csi.ProbeResponse{}, nil
+}
+
+func (driver *Driver) getIscsiInfoPath(volumeID string) string {
+	return fmt.Sprintf("%s/plugins/%s/iscsi-%s.json", driver.kubeletPath, common.PluginName, volumeID)
 }
 
 func isKernelModLoaded(modName string) bool {
